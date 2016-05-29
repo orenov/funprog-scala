1c7d1fc60d810c5e95d0b4c39aa898c1c807defc
oleksii renov <forihcs@gmail.com>
2016-05-29 03:08:49 +0300

    recfuns

---
+++ functional-programming-principles-in-scala/week1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,48 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) return 1
+    pascal(c, r - 1) + pascal(c, r)
+  }
+  
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def balanced(chars : List[Char], queue : Int) : Boolean = {
+      if (chars.isEmpty) queue == 0
+      else {
+        chars.head match {
+          case '(' => balanced(chars.tail, queue + 1)
+          case ')' => balanced(chars.tail, queue - 1)
+          case _ => balanced(chars.tail, queue)
+        }
+      }
+    }
+
+    balanced(chars, 0)
+  }
+  
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if(money > 0 && coins.nonEmpty)
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else 0
+  }
+}
